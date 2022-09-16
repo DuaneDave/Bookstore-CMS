30c074f5c9c38081498935371fed8a6e5a6193a2
Duane David <duanedave17@gmail.com>
2022-09-16 02:29:55 -0700

    Add Header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <div className="header">
+      <h1>Bookstore CMS</h1>
+      <nav>
+        <ul>
+          <li>Books</li>
+          <li>Categories</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
