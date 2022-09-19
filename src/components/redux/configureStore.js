1215cc63d8753cc012d44fbe57ade46746d70265
Duane David <duanedave17@gmail.com>
2022-09-19 04:20:16 -0700

    Update configureStore.js

--- src/components/redux/configureStore.js
+++ src/components/redux/configureStore.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import booksReducer from './books/book';
 import categoriesReducer from './categories/categories';
-
 const store = configureStore({
   reducer: {
     books: booksReducer,
