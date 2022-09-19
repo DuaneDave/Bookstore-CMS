da8446b0694c7bac7969a9cb1e17ff5087b9dd3b
Duane David <duanedave17@gmail.com>
2022-09-19 04:19:24 -0700

    Update book.js

--- src/components/redux/books/book.js
+++ src/components/redux/books/book.js
@@ -5,10 +5,8 @@ const booksReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
-
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload);
-
     default:
       return state;
   }
