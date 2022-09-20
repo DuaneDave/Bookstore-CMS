45d82b125fa2ff5d18ed339d92f6f1fb8379aea4
Duane David <duanedave17@gmail.com>
2022-09-20 06:40:52 -0700

    Reconfigure input component

--- src/UI/Input.jsx
+++ src/UI/Input.jsx
@@ -1,6 +1,14 @@
-function Input({ type, placeholder }) {
+function Input({
+  type, placeholder, onUpdate, value,
+}) {
   return (
-    <input type={type} aria-label={placeholder} placeholder={placeholder} />
+    <input
+      type={type}
+      aria-label={placeholder}
+      placeholder={placeholder}
+      onChange={onUpdate}
+      value={value}
+    />
   );
 }
 
