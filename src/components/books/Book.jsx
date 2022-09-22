e79b32a6e16c6cd7fef231bea6e3ed84e778d2fc
Duane David <duanedave17@gmail.com>
2022-09-22 07:21:08 -0700

    Add style for book component

--- src/components/books/Book.jsx
+++ src/components/books/Book.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import Button from '../../UI/Button';
 import { removeBook } from '../../redux/books/book';
+import styles from './Book.module.css';
 
 function Book({ title, author, index }) {
   const dispatch = useDispatch();
@@ -10,25 +11,34 @@ function Book({ title, author, index }) {
   };
 
   return (
-    <div>
-      <div>
+    <div className={styles.book}>
+      <div className={styles.details}>
+        <p>Action</p>
         <h3>{title}</h3>
-        <p>{author}</p>
-        <div>
+        <h5>{author}</h5>
+        <div className={styles.actions}>
           <span>Comments</span>
           <span>Remove</span>
           <span>Edit</span>
         </div>
       </div>
 
-      <div>
-        <span>Current Chapter</span>
-        <span>Chapter 17</span>
-        <Button
-          type="button"
-          title="Remove Book"
-          onAction={removeBookHandler}
-        />
+      <div className={styles.progress}>
+        <h2>80%</h2>
+        <span>Completed</span>
+      </div>
+
+      <div className={styles.info}>
+        <p>CURRENT CHAPTER</p>
+        <h2>Chapter 17</h2>
+        <span className={styles.btn}>
+          <Button
+            type="button"
+            title="REMOVE BOOK"
+            onAction={removeBookHandler}
+            classes={styles.remove}
+          />
+        </span>
       </div>
     </div>
   );
